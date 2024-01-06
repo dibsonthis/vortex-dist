6b94fa23a5779b4ee860309ad8b2b20b52521049
Adib <adib.attie33@gmail.com>
2024-01-07 02:40:53 +1100

    added windows scripts

---
+++ win/scaffold.bat
@@ -0,0 +1,94 @@
+@echo off
+
+:: Function to scaffold the project
+:scaffold
+    mkdir modules
+    mkdir src
+
+    echo.import time > "src/main.vtx"
+    echo.import [] : string >> "src/main.vtx"
+    echo.import [] : functional >> "src/main.vtx"
+    echo. >> "src/main.vtx"
+    echo.const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"] >> "src/main.vtx"
+    echo. >> "src/main.vtx"
+    echo.const datetime = time.datetime() >> "src/main.vtx"
+    echo.const datetime_split = datetime.split().filter^(^(e^) =^> e != ""^) >> "src/main.vtx"
+    echo.const datetime_obj = {^ >> "src/main.vtx"
+    echo.    dayname: datetime_split[0],^ >> "src/main.vtx"
+    echo.    monthname: datetime_split[1],^ >> "src/main.vtx"
+    echo.    day: datetime_split[2],^ >> "src/main.vtx"
+    echo.    month: months.indexOf(datetime_split[1]) + 1,^ >> "src/main.vtx"
+    echo.    year: datetime_split[4],^ >> "src/main.vtx"
+    echo.    time: datetime_split[3],^ >> "src/main.vtx"
+    echo.} >> "src/main.vtx"
+    echo. >> "src/main.vtx"
+    echo.println(datetime_obj) >> "src/main.vtx"
+
+    echo.# Ignore interpreter and modules dir > ".gitignore"
+    echo.vortex >> ".gitignore"
+    echo.modules/ >> ".gitignore"
+    echo..DS_Store >> ".gitignore"
+
+    echo.; Vortex Configuration > "config.ini"
+    echo.>> "config.ini"
+    echo.; entry: Sets the entry point of the program from root >> "config.ini"
+    echo.; modules: Sets the modules folder - leave as an empty string to use system installed modules >> "config.ini"
+    echo.>> "config.ini"
+    echo.entry="src/main.vtx">> "config.ini"
+    echo.modules="modules">> "config.ini"
+
+    echo.@echo off > "run.bat"
+    echo.setlocal enabledelayedexpansion >> "run.bat"
+    echo. >> "run.bat"
+    echo.rem Load config.ini values >> "run.bat"
+    echo.for /f "usebackq tokens=1,* delims==" %%%%i in ("config.ini") do (^ >> "run.bat"
+    echo.    set "%%%%i=%%%%~j"^ >> "run.bat"
+    echo.) >> "run.bat"
+    echo. >> "run.bat"
+    echo.if not defined modules (^ >> "run.bat"
+    echo.    vortex.exe !entry!^ >> "run.bat"
+    echo.) else (^ >> "run.bat"
+    echo.    vortex.exe !entry! -m !CD!\!modules!^ >> "run.bat"
+    echo.) >> "run.bat"
+    echo. >> "run.bat"
+    echo.endlocal >> "run.bat"
+
+    :: Download and unzip interpreter and modules
+    curl -LO -k "https://github.com/dibsonthis/Vortex/releases/latest/download/windows.zip"
+
+    powershell Expand-Archive "windows.zip" "windows"
+
+    move "windows\vortex.exe" "vortex.exe"
+
+    if not exist "modules.txt" (
+        (
+            echo.functional
+            echo.functools
+            echo.future
+            echo.io
+            echo.json
+            echo.logging
+            echo.math
+            echo.os
+            echo.requests
+            echo.sdl
+            echo.string
+            echo.sys
+            echo.time
+        ) > "modules.txt"
+    )
+
+    cd modules
+    for /f "usebackq delims=" %%a in ("..\modules.txt") do (
+        set "trimmed=%%a"
+        setlocal enabledelayedexpansion
+        move "..\windows\modules\!trimmed!" "!trimmed!"
+    )
+    cd ..
+
+    rmdir /s /q "windows"
+    del /q "windows.zip"
+goto :eof
+
+:: Call the scaffold function
+call :scaffold
---
+++ win/update.bat
@@ -0,0 +1,62 @@
+@echo off
+
+:: Function to update the project
+:update
+
+    :: Download and unzip interpreter and modules
+    curl -LO -k "https://github.com/dibsonthis/Vortex/releases/latest/download/windows.zip"
+
+    powershell Expand-Archive "windows.zip" "windows"
+
+    if exist modules (
+        rmdir /s /q modules
+        echo Deleting modules...
+    ) else (
+        echo.
+    )
+
+    if exist vortex.exe (
+        echo Deleting interpreter...
+        del vortex.exe
+    ) else (
+        echo.
+    )
+
+    move windows\vortex.exe vortex.exe
+
+    if exist modules.txt (
+        echo.
+    ) else (
+        echo functional> modules.txt
+        echo functools>> modules.txt
+        echo future>> modules.txt
+        echo io>> modules.txt
+        echo json>> modules.txt
+        echo logging>> modules.txt
+        echo math>> modules.txt
+        echo os>> modules.txt
+        echo requests>> modules.txt
+        echo sdl>> modules.txt
+        echo string>> modules.txt
+        echo sys>> modules.txt
+        echo time>> modules.txt
+    )
+
+    mkdir modules
+    cd modules
+
+    for /f "usebackq delims=" %%a in ("..\modules.txt") do (
+        set "trimmed=%%a"
+        setlocal enabledelayedexpansion
+        move "..\windows\modules\!trimmed!" "!trimmed!"
+    )
+
+    cd ..
+
+    rmdir /s /q windows
+    del windows.zip
+
+goto :eof
+
+:: Call the update function
+call :update
